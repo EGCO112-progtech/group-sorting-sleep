ae9de2f1fe089b3dc5615514cf89c80dd8799792
thun3032 <122582065+thun3032@users.noreply.github.com>
2023-02-03 12:21:56 +0000

    bubble

--- sorting.h
+++ sorting.h
@@ -57,7 +57,7 @@ void bubbleSort(int a[],int n){
   int sorted;
   for (i = 0; i < n ; i++){
       sorted = 0;
-      for (j = 0; j < n - 1 ; j++) {
+      for (j = 0; j < n - 1-i ; j++) {
         if (a[j] < a[j + 1]) {
           swap(&a[j], &a[j + 1]);
           sorted = 1;
