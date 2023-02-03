e67cbe2704d9d2f1b35f364a55846ef2642fe8ab
puthipongyomabu <123081303+puthipongyomabut@users.noreply.github.com>
2023-02-03 10:31:15 +0000

    bubble

---
+++ .ccls-cache/@home@runner@group-sorting-sleep/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sorting.h"
+
+int main(int argc,char **argv) {
+  int i,n,*a;
+  n=argc-2;
+  a=(int*)malloc(sizeof(int)*n);
+  for(i=0;i<n;i++){
+    a[i]=atoi(argv[i+2]);
+  }
+  display(a,n);
+  printf("\n");
+  if(strcmp(argv[1],"bubble")==0)
+      bubbleSort(a,n);
+  else  if(strcmp(argv[1],"insertion")==0)
+      insertionSort(a,n);
+  else  if(strcmp(argv[1],"selection")==0) 
+         selectionSort(a,n);
+  else {
+    printf("Incorrect type\n");
+  }
+  printf("\n");
+  display(a,n);
+ return 0;
+}
+
+
